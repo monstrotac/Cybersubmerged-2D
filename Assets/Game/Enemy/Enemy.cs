e75776c1f2a2feda9268b1a9fdfbeee6a352a8da
Benjamin_Robert <46529441+TinyTheRealCoder@users.noreply.github.com>
2023-03-18 10:08:40 -0400

    update niv 1

--- Assets/Game/Enemy/Enemy.cs
+++ Assets/Game/Enemy/Enemy.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public List<Vector2> patrol;
+    public int patrolstep = 0;
+
+    public float timer = 0;
+    public float speed = 1;
+    public float dist = 1;
+
+    public bool isPatroling = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,17 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (isPatroling && patrol.count > 0)
+        {
+            transform.position = new Vector3(Mathf.Lerp(patrol[calculateNextPoisitionIndex(step)].x, patrol[calculateNextPoisitionIndex(step+1)].x, timer/dist), Mathf.Lerp(), transform.position.z);
+        }
+    }
+
+    public int calculateNextPoisitionIndex(int index) {
+        if (index >= patrol.count) {
+            return index-patrol.count;
+        }
+        return index;
     }
 }
